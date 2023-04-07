eecbdd53ef4d0b379eba8eb509d66ed3c888d147
Mikaela Hammel <mikaela.hammel@kellogg.northwestern.edu>
2023-04-07 20:10:02 +0000

    finished lab 3

--- lab-3-3.sql
+++ lab-3-3.sql
@@ -1,6 +1,14 @@
 -- What were the winningest teams in each season of the 
 -- modern era (from 1960-present), listed by winningest teams first?
 
+SELECT year, name, MAX(wins)
+FROM teams
+WHERE year > 1959
+GROUP BY name
+ORDER BY wins DESC
+;
+
+
 -- Expected result: 61 rows, starting with
 --
 -- +------+-------------------------------+-----------+
