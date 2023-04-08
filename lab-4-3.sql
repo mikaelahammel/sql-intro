2162749d0a77cad570810fb27b370acb021a3a57
Mikaela Hammel <mikaela.hammel@kellogg.northwestern.edu>
2023-04-08 02:26:40 +0000

    finished 4-3

--- lab-4-3.sql
+++ lab-4-3.sql
@@ -1,5 +1,13 @@
 -- Who hit the most home runs in 2019, and what team did they play for?
 
+-- need to confirm if MAX is correct or just worked in this instance
+SELECT teams.name, players.first_name, players.last_name, MAX(stats.home_runs)
+FROM stats
+    INNER JOIN teams ON stats.team_id = teams.id
+    INNER JOIN players ON stats.player_id = players.id
+WHERE teams.year = 2019
+;
+
 -- Expected result:
 --
 -- +---------------+------------+-----------+-----------+
