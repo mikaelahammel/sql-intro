adf2fed938c0011e4d2f6a8106dd1666ed8bded7
Mikaela Hammel <mikaela.hammel@kellogg.northwestern.edu>
2023-04-08 02:30:33 +0000

    finished 4-4

--- lab-4-4.sql
+++ lab-4-4.sql
@@ -1,5 +1,13 @@
 -- Who was the leading home run hitter for each team in 2019?
 
+SELECT teams.name, players.first_name, players.last_name, MAX(stats.home_runs)
+FROM stats
+    INNER JOIN teams ON stats.team_id = teams.id
+    INNER JOIN players ON stats.player_id = players.id
+WHERE teams.year = 2019
+GROUP BY teams.name
+;
+
 -- Expected result:
 --
 -- +-------------------------------+------------+-------------+----------------------+
